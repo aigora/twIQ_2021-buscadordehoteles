869584b0d4c76a6cc9cf3cf5ab6aa172a2476dd1
anaboston <ana.boston@alumnos.upm.es>
2021-04-30 00:25:51 +0200

    Ficheros

--- buscadordehoteles.c
+++ buscadordehoteles.c
@@ -13,12 +13,26 @@ struct reserva {
 	float precio;
 };
 
+//Menu del trabajo:
 int main() {
 	char opcion;
 	char opcion2;
-	int i, dato, contador=0, comp;
-	FILE * fregistro;
+	int i, dato, contador=0, comparador;
 	struct usuario usuarios[100];
+	
+	//Leer fichero .txt
+	FILE * fregistro;
+	fregistro=fopen("Registro.txt","r");
+	if(fregistro == NULL) {
+		printf("No se ha podido abrir el fichero Registro correctamente\n");
+		return 0;
+	}
+	while(fscanf(fregistro,"%d %s %d", &contador+1, usuarios[contador].nombre, usuarios[contador].clave) != EOF) {
+		printf("%d %s %d", contador+1, usuarios[contador].nombre, usuarios[contador].clave);
+		contador++;
+	}	
+	contador = i;		
+	fclose(fregistro);
 	
 	printf("----------------------\n");
 	printf("\tMenu:\n");
@@ -34,133 +48,63 @@ int main() {
 	switch(opcion) {
 		case 'r':
 		case 'R':
-			fregistro=fopen("Registro.txt","r");
-			
-			if(fregistro == NULL) {
-				printf("No se ha podido abrir el fichero Registro correctamente\n");
-				return 0;
-			}
-			
-			while(fscanf(fregistro,"%d %s %s %d", &contador+1, usuarios[contador].nombre, usuarios[contador].clave, &dato) != EOF) {
-				printf("%d %s %s %d", contador+1, usuarios[contador].nombre, usuarios[contador].clave, dato);
-				contador++;
-			}
+		
 			
 			i=contador+1;
 				
 			do{
+				//Comprobar si ya existe el usuario:
 				printf("Introduzca el nombre de usuario:\n");
 				scanf("%s",usuarios[i].nombre);
+				fflush(stdin);
 				
 				for(contador=0; contador<i; contador++) {
-					comp=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
-					if(comp==0) {
+					comparador=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
+					if(comparador==0) {
 						printf("El nombre de usuario introducido ya existe\n");
 						break;
 					}
 					
-					else if(comp==1) {
+					else if(comparador==1) {
 						printf("Se ha registrado el nombre introducido\n");
 						break;
 					}
 					
 				}
 			}
-			while(comp=0);			
-						
-			fclose(fregistro);
+			while(comparador=0);
 			
-			fregistro=fopen("Registro.txt","w");
-			if(fregistro==NULL) {
-				printf("No se ha podido abrir el fichero Registro correctamente\n");
-				return 0;
+			if (comparador= 0) {
+					strcpy(usuarios[contador].nombre, usuarios[i].nombre);
 			}
-			printf("Introduzca su clave:\n");
+            fflush(stdin);
+			
+            printf("Introduzca su clave:\n");
 			scanf("%d", &usuarios[i].clave);
-			printf("%d %s %d\n", i, usuarios[i].nombre, usuarios[i].clave);
-			for(i=0; i<=contador; i++) {
-				fprintf(fregistro, "%d %s %d\n", i, usuarios[i].nombre, usuarios[i].clave);
-				printf("%d %s %d\n", i, usuarios[i].nombre, usuarios[i].clave);
-			}
-			fclose(fregistro);
+			
+					
+			printf("Se ha registrado correctamente\n");
 			break;			
-				
-
-			printf("--------------------------\n");
-			printf("Que desea hacer?:\n");
-			printf("R--Hacer una reserva\n");
-			printf("H--Buscar hoteles\n");
-			printf("B--Borrar reserva\n");
-			printf("C--Consultar Reserva\n");
-			printf("A--Seccion de comentarios\n");
-			printf("S--Salir del programa\n");
-			printf("--------------------------\n");
 			
-			
-				
-			do{
-				printf("Introduzca la opcion: \n");
-				fflush(stdin);
-				scanf("%c", &opcion2);
-				switch(opcion2) {
-					case 'r':
-					case 'R':
-						printf("Hacer una reserva\n");
-						break;
-					case 'h':
-					case 'H':
-						printf("Buscar hoteles\n");
-						break;
-					case 'b':
-					case 'B':
-						printf("Borrar reserva\n");
-						break;
-					case 'c':
-					case 'C':
-						printf("Consultar reserva\n");
-						break;
-					case 'a':
-					case 'A':
-						printf("Seccion de comentarios\n");
-						break;			
-					case 's':
-					case 'S':
-						printf("Gracias por utilizar este programa\n");
-						return 0;
-					default:
-						printf("La opcion es incorrecta\n");			
-					}
-			} while (opcion2 != 'S');
-			break;
 		case 'i':
 		case 'I':
-			
-			fregistro=fopen("Registro.txt","r");
-			
-			if(fregistro == NULL) {
-				printf("No se ha podido abrir el fichero Registro correctamente\n");
-				return 0;
-			}
-			
-			
-			while(fscanf(fregistro,"%d %s %s %d", &contador+1, usuarios[contador].nombre, usuarios[contador].clave, &dato) != EOF) {
-				printf("%d %s %s %d", contador+1, usuarios[contador].nombre, usuarios[contador].clave, dato);
-				contador++;
-			}
 			
 			i=contador+1;
 				
 			do{
+				//Comprobamos que el usuario exista:
 				printf("Introduzca el nombre de usuario:\n");
 				scanf("%s",usuarios[i].nombre);
+				fflush(stdin);
 				
-				for(contador=0; contador<=i; contador++) {
-					comp=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
-					if(comp==0) {
+				for(contador=0; contador<i; contador++) {
+					comparador=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
+					if(comparador==0) {
 						do {
+							//Comprobamos que la contraseña sea correcta
 							printf("Introduzca su clave para acceder:\n");
 							scanf("%d", usuarios[i].clave);
-							for(contador=0;contador<=i; contador++) {
+							for(contador=0;contador<i; contador++) {
 								if(usuarios[i].clave == usuarios[contador].clave) {
 									printf("Te damos la bienvenida a Buscador de Hoteles\n");
 									break;
@@ -175,67 +119,81 @@ int main() {
 						break;
 					}
 					
-					else if(comp==1) {
+					else if(comparador==1) {
 						printf("Usuario no existente, por favor intentelo de nuevo.\n");
 						break;
 					}
 					
 				}
 			}
-			while(comp==1);
-			fclose(fregistro);
+			while(comparador==1);
+			
 			break;	
 			
-			
-			printf("--------------------------\n");
-			printf("Que desea hacer?:\n");
-			printf("R--Hacer una reserva\n");
-			printf("H--Buscar hoteles\n");
-			printf("B--Borrar reserva\n");
-			printf("C--Consultar Reserva\n");
-			printf("A--Seccion de comentarios\n");
-			printf("S--Salir del programa\n");
-			printf("--------------------------\n");
-				
-				do{
-					printf("Introduzca la opcion: \n");
-					fflush(stdin);
-					scanf("%c", &opcion2);
-					switch(opcion2) {
-						case 'r':
-						case 'R':
-							printf("Hacer una reserva\n");
-							break;
-						case 'h':
-						case 'H':
-							printf("Buscar hoteles\n");
-							break;
-						case 'b':
-						case 'B':
-							printf("Borrar reserva\n");
-							break;
-						case 'c':
-						case 'C':
-							printf("Consultar reserva\n");
-							break;
-						case 'a':
-						case 'A':
-							printf("Seccion de comentarios\n");
-							break;			
-						case 's':
-						case 'S':
-							printf("Gracias por utilizar este programa\n");
-							return 0;
-						default:
-							printf("La opcion es incorrecta\n");			
-					}
-				} while (opcion2 != 'S');
+		case 's':
+		case 'S':
+			printf("Gracias por utilizar este programa\n");
+			return 0;
+		default:
+			printf("La opcion es incorrecta\n");
+			return 0;			
+		}
+	//Guardar la memoria en el .txt
+    		fregistro = fopen("Registro.txt", "w"); 
+    		if(fregistro==NULL) {
+				printf("No se ha podido abrir el fichero Registro correctamente\n");
+				return 0;
+			}
+    		for (contador= 0; contador < i; contador++) {
+    			fprintf(fregistro, "%s %d\n", usuarios[i].nombre, usuarios[i].clave);
+			}
+    		fclose(fregistro);
+    		fflush(stdin);
+	printf("--------------------------\n");
+	printf("Que desea hacer?:\n");
+	printf("R--Hacer una reserva\n");
+	printf("H--Buscar hoteles\n");
+	printf("B--Borrar reserva\n");
+	printf("C--Consultar Reserva\n");
+	printf("A--Seccion de comentarios\n");
+	printf("S--Salir del programa\n");
+	printf("--------------------------\n");
+	
+	
+		
+	do{
+		printf("Introduzca la opcion: \n");
+		fflush(stdin);
+		scanf("%c", &opcion2);
+		switch(opcion2) {
+			case 'r':
+			case 'R':
+				printf("Hacer una reserva\n");
 				break;
+			case 'h':
+			case 'H':
+				printf("Buscar hoteles\n");
+				break;
+			case 'b':
+			case 'B':
+				printf("Borrar reserva\n");
+				break;
+			case 'c':
+			case 'C':
+				printf("Consultar reserva\n");
+				break;
+			case 'a':
+			case 'A':
+				printf("Seccion de comentarios\n");
+				break;			
 			case 's':
 			case 'S':
 				printf("Gracias por utilizar este programa\n");
 				return 0;
 			default:
 				printf("La opcion es incorrecta\n");			
-		}
+			}
+	} while (opcion2 != 'S');
+	
+	
 	}
