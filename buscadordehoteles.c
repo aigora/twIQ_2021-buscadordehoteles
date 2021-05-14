2d204e989b4bb2f775fc76ed9f6ae2078b4c085f
anaboston <ana.boston@alumnos.upm.es>
2021-05-15 00:07:07 +0200

    .
    Solucionado el problema de inicio de sesión

--- buscadordehoteles.c
+++ buscadordehoteles.c
@@ -2,7 +2,7 @@
 #include <string.h>
 struct usuario {
 	char nombre[100];
-	int clave;
+	char clave[10];
 };
 struct reserva {
 	int dia;
@@ -17,22 +17,25 @@ struct reserva {
 int main() {
 	char opcion;
 	char opcion2;
-	int i, dato, contador=0, comparador;
+	int i, dato, nUsuarios=0, comparador, comparador2;
 	struct usuario usuarios[100];
 	
 	//Leer fichero .txt
-	FILE * fregistro;
-	fregistro=fopen("Registro.txt","r");
-	if(fregistro == NULL) {
+	FILE * pregistro;
+	pregistro=fopen("Registro.txt","r");
+	if(pregistro == NULL) {
 		printf("No se ha podido abrir el fichero Registro correctamente\n");
 		return 0;
 	}
-	while(fscanf(fregistro,"%d %s %d", &contador+1, usuarios[contador].nombre, usuarios[contador].clave) != EOF) {
-		printf("%d %s %d", contador+1, usuarios[contador].nombre, usuarios[contador].clave);
-		contador++;
+	while(fscanf(pregistro,"%s %s", usuarios[nUsuarios].nombre, usuarios[nUsuarios].clave) != EOF) {
+		//printf("%s %d", usuarios[contador].nombre, usuarios[contador].clave);
+		nUsuarios++;
 	}	
-	contador = i;		
-	fclose(fregistro);
+	nUsuarios = i;		
+	fclose(pregistro);
+	
+	
+	//menu principal
 	
 	printf("----------------------\n");
 	printf("\tMenu:\n");
@@ -49,38 +52,34 @@ int main() {
 		case 'r':
 		case 'R':
 		
+			//Registro de nuevo usuario
+			i=nUsuarios+1;
 			
-			i=contador+1;
-				
 			do{
 				//Comprobar si ya existe el usuario:
 				printf("Introduzca el nombre de usuario:\n");
 				scanf("%s",usuarios[i].nombre);
 				fflush(stdin);
 				
-				for(contador=0; contador<i; contador++) {
-					comparador=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
+				for(nUsuarios=0; nUsuarios<i; nUsuarios++) {
+					comparador=strcmp(usuarios[nUsuarios].nombre, usuarios[i].nombre);
 					if(comparador==0) {
 						printf("El nombre de usuario introducido ya existe\n");
 						break;
-					}
-					
-					else if(comparador==1) {
+					} else if(comparador!=0) {
 						printf("Se ha registrado el nombre introducido\n");
 						break;
 					}
-					
 				}
-			}
-			while(comparador=0);
+			}while(comparador==0);
 			
-			if (comparador= 0) {
-					strcpy(usuarios[contador].nombre, usuarios[i].nombre);
+			if (comparador== 0) {
+					strcpy(usuarios[nUsuarios].nombre, usuarios[i].nombre);
 			}
             fflush(stdin);
-			
+			//Clave
             printf("Introduzca su clave:\n");
-			scanf("%d", &usuarios[i].clave);
+			scanf("%s", &usuarios[i].clave);
 			
 					
 			printf("Se ha registrado correctamente\n");
@@ -89,7 +88,8 @@ int main() {
 		case 'i':
 		case 'I':
 			
-			i=contador+1;
+			//Inicio de sesión
+			i=nUsuarios+1;
 				
 			do{
 				//Comprobamos que el usuario exista:
@@ -97,36 +97,33 @@ int main() {
 				scanf("%s",usuarios[i].nombre);
 				fflush(stdin);
 				
-				for(contador=0; contador<i; contador++) {
-					comparador=strcmp(usuarios[contador].nombre, usuarios[i].nombre);
+				for(nUsuarios=0; nUsuarios<i; nUsuarios++) {
+					comparador=strcmp(usuarios[nUsuarios].nombre, usuarios[i].nombre);
 					if(comparador==0) {
 						do {
 							//Comprobamos que la contraseña sea correcta
 							printf("Introduzca su clave para acceder:\n");
-							scanf("%d", usuarios[i].clave);
-							for(contador=0;contador<i; contador++) {
-								if(usuarios[i].clave == usuarios[contador].clave) {
+							scanf("%s", usuarios[i].clave);
+							for(nUsuarios=0;nUsuarios<i; nUsuarios++) {
+								comparador2=strcmp(usuarios[nUsuarios].clave, usuarios[i].clave);
+								if(comparador2==0) {
 									printf("Te damos la bienvenida a Buscador de Hoteles\n");
 									break;
-								}
-								else {
+								} else {
 									printf("Clave incorrecta, por favor intentelo de nuevo.\n");
 									break;
 								}
 							}
-						}
-						while(usuarios[i].clave != usuarios[contador].clave);
+						} while(comparador2!=0);
+						
 						break;
-					}
-					
-					else if(comparador==1) {
+					} else if(comparador==1) {
 						printf("Usuario no existente, por favor intentelo de nuevo.\n");
 						break;
 					}
 					
 				}
-			}
-			while(comparador==1);
+			} while(comparador!=0);
 			
 			break;	
 			
@@ -139,15 +136,15 @@ int main() {
 			return 0;			
 		}
 	//Guardar la memoria en el .txt
-    		fregistro = fopen("Registro.txt", "w"); 
-    		if(fregistro==NULL) {
+    		pregistro = fopen("Registro.txt", "w"); 
+    		if(pregistro==NULL) {
 				printf("No se ha podido abrir el fichero Registro correctamente\n");
 				return 0;
 			}
-    		for (contador= 0; contador < i; contador++) {
-    			fprintf(fregistro, "%s %d\n", usuarios[i].nombre, usuarios[i].clave);
+    		for (nUsuarios= 0; nUsuarios < i; nUsuarios++) {
+    			fprintf(pregistro, "%s %s\n", usuarios[i].nombre, usuarios[i].clave);
 			}
-    		fclose(fregistro);
+    		fclose(pregistro);
     		fflush(stdin);
 	printf("--------------------------\n");
 	printf("Que desea hacer?:\n");
